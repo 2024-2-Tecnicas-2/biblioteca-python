7a3a06660961ce1c6d23f182b05673d84222d84c
Daniel Sanchez <daniel.sanchez@apriorins.com>
2024-09-05 20:08:39 -0500

    Primer commit

---
+++ src/principal.py
@@ -0,0 +1,2 @@
+if __name__ == '__main__':
+    # TODO: Aquí va el código que inicializa tu aplicación.
---
+++ tests/test_publicacion.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import unittest
+
+# Add the 'src' directory to the Python path so 'calculadora' can be imported
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+class TestPublicacion(unittest.TestCase):
+    # TODO Adiciona tus pruebas unitarias aquí.
+    # Ejemplo:
+    '''
+    def test_multiplicar_positivos(self):
+        valor_esperado = 15
+        mi_cuenta = CuentaBancaria()
+        valor_actual = mi_cuenta.multiplicar(3, 5)
+        self.assertEqual(valor_esperado, valor_actual)
+    '''
+        
+if __name__ == '__main__':
+    unittest.main()
